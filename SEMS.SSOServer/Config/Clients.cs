faf5f8630a7144cd47702aebed68fb0973bac1a2
hqpsoft <hqpsoft@outlook.com>
2015-12-16 23:49:00 +0800

    add sso

---
+++ SEMS.SSOServer/Config/Clients.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Security.Claims;
+using IdentityServer3.Core;
+using IdentityServer3.Core.Models;
+
+namespace SEMS.SSOServer.Config.Config
+{
+    public class Clients
+    {
+        public static List<Client> Get()
+        {
+            return new List<Client>
+            {
+                new Client
+                {
+                    ClientName = "SEMS",
+                    ClientId = "SEMS",
+                    Flow = Flows.Implicit,
+                    RequireConsent = false,
+                    RedirectUris = new List<string>
+                                    {
+                                        "http://localhost:8896/",
+                                    },
+
+                    PostLogoutRedirectUris = new List<string>
+                                            {
+                                                "http://localhost:8896/",
+                                            },
+
+                    AllowedScopes = new List<string>
+                                    {
+                                        "openid",
+                                        "profile",
+                                        "roles",
+                                        "sampleApi"
+                                    },
+                },
+            };
+        }
+    }
+}
