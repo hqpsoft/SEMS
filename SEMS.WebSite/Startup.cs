f6ecf0d5d201d015775269191c71d94125544ead
hqpsoft <hqpsoft@outlook.com>
2015-12-23 21:45:44 +0800

    SSO集成

---
+++ SEMS.WebSite/Startup.cs
@@ -0,0 +1,83 @@
+﻿using IdentityModel.Client;
+using Microsoft.IdentityModel.Protocols;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.OpenIdConnect;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Helpers;
+
+namespace SEMS.WebSite
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = OpenIdConnectAuthenticationDefaults.AuthenticationType,
+            });
+
+
+            app.UseOpenIdConnectAuthentication(new OpenIdConnectAuthenticationOptions
+            {
+                Authority = "http://localhost:8896/identity",
+
+                ClientId = "SEMS",
+                Scope = "openid profile",
+                ResponseType = "id_token token",
+                RedirectUri = "http://localhost:60544/",
+                PostLogoutRedirectUri= "http://localhost:60544/",
+                SignInAsAuthenticationType = OpenIdConnectAuthenticationDefaults.AuthenticationType,
+                UseTokenLifetime = false,
+
+                Notifications = new OpenIdConnectAuthenticationNotifications
+                {
+                    SecurityTokenValidated = async n =>
+                    {
+                        var nid = new ClaimsIdentity(n.AuthenticationTicket.Identity.AuthenticationType, "name", "role");
+
+                        // get userinfo data
+                        var userInfoClient = new UserInfoClient(
+                        new Uri(n.Options.Authority + "/connect/userinfo"),
+                        n.ProtocolMessage.AccessToken);
+
+                        var userInfo = await userInfoClient.GetAsync();
+                        userInfo.Claims.ToList().ForEach(ui => nid.AddClaim(new Claim(ui.Item1, ui.Item2)));
+
+                        // keep the id_token for logout
+                        nid.AddClaim(new Claim("id_token", n.ProtocolMessage.IdToken));
+                        // add access token for sample API
+                        nid.AddClaim(new Claim("access_token", n.ProtocolMessage.AccessToken));
+                        // keep track of access token expiration
+                        nid.AddClaim(new Claim("expires_at", DateTimeOffset.Now.AddSeconds(int.Parse(n.ProtocolMessage.ExpiresIn)).ToString()));
+                        // add some other app specific claim
+                        nid.AddClaim(new Claim("app_specific", "some data"));
+                        n.AuthenticationTicket = new AuthenticationTicket(nid, n.AuthenticationTicket.Properties);
+                    },
+
+                    RedirectToIdentityProvider = n =>
+                    {
+                        if (n.ProtocolMessage.RequestType == OpenIdConnectRequestType.LogoutRequest)
+                        {
+                            var idTokenHint = n.OwinContext.Authentication.User.FindFirst("id_token");
+
+                            if (idTokenHint != null)
+                            {
+                                n.ProtocolMessage.IdTokenHint = idTokenHint.Value;
+                            }
+                        }
+
+                        return Task.FromResult(0);
+                    }
+                }
+            });
+        }
+    }
+}
