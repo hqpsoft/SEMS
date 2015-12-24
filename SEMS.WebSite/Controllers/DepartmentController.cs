06837b834d3086883d80ec333222c50dcf50923c
sumahe <sumahe2007@hotmail.com>
2015-12-24 18:02:10 +0800

    补全DepartmentController

---
+++ SEMS.WebSite/Controllers/DepartmentController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SEMS.WebSite.Controllers
+{
+    public class DepartmentController : BaseController
+    {
+        //
+        // GET: /Department/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Form()
+        {
+            return View();
+        }
+	}
+}
